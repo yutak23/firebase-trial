db65135a1a0a9c6766e461e3c91da70f879b65ff
Yuta Katayama <yutapilafu@gmail.com>
2023-03-20 13:02:21 +0900

    chore: firebase init functionsとESLint, Prettierの設定

---
+++ functions/.eslintrc.cjs
@@ -0,0 +1,20 @@
+require('@rushstack/eslint-patch/modern-module-resolution');
+
+module.exports = {
+	parserOptions: {
+		ecmaVersion: 2023
+	},
+	extends: ['eslint:recommended', 'google', 'airbnb-base', 'prettier'],
+	rules: {
+		'import/no-extraneous-dependencies': ['warn', { packageDir: './' }]
+	},
+	overrides: [
+		{
+			files: ['**/*.spec.*'],
+			env: {
+				mocha: true
+			},
+			rules: {}
+		}
+	]
+};

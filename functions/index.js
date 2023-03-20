fa06d1c34495def3a5668296fa93ab11feb92970
Yuta Katayama <yutapilafu@gmail.com>
2023-03-20 20:29:00 +0900

    feat: addMessage, makeUppercaseを実装

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,25 @@
-const functions = require('firebase-functions');
+import functions from 'firebase-functions';
+import admin from 'firebase-admin';
 
-// // Create and deploy your first functions
-// // https://firebase.google.com/docs/functions/get-started
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+export const addMessage = functions.https.onRequest(async (req, res) => {
+	const original = req.query.text;
+
+	const writeResult = await admin
+		.firestore()
+		.collection('messages')
+		.add({ original });
+
+	res.json({ result: `Message with ID: ${writeResult.id} added.` });
+});
+
+export const makeUppercase = functions.firestore
+	.document('/messages/{documentId}')
+	.onCreate((snap, context) => {
+		const { original } = snap.data();
+		functions.logger.log('Uppercasing', context.params.documentId, original);
+
+		const uppercase = original.toUpperCase();
+		return snap.ref.set({ uppercase }, { merge: true });
+	});

ee0b97f1c3011a871b9f792ea502038398da6c4a
Yuta Katayama <yutapilafu@gmail.com>
2023-03-21 22:15:41 +0900

    chore: deployを実際にしてみる

--- functions/index.js
+++ functions/index.js
@@ -3,19 +3,22 @@ import admin from 'firebase-admin';
 
 admin.initializeApp();
 
-export const addMessage = functions.https.onRequest(async (req, res) => {
-	const original = req.query.text;
+export const addMessage = functions
+	.region('asia-northeast1')
+	.https.onRequest(async (req, res) => {
+		const original = req.query.text;
 
-	const writeResult = await admin
-		.firestore()
-		.collection('messages')
-		.add({ original });
+		const writeResult = await admin
+			.firestore()
+			.collection('messages')
+			.add({ original });
 
-	res.json({ result: `Message with ID: ${writeResult.id} added.` });
-});
+		res.json({ result: `Message with ID: ${writeResult.id} added.` });
+	});
 
-export const makeUppercase = functions.firestore
-	.document('/messages/{documentId}')
+export const makeUppercase = functions
+	.region('asia-northeast1')
+	.firestore.document('/messages/{documentId}')
 	.onCreate((snap, context) => {
 		const { original } = snap.data();
 		functions.logger.log('Uppercasing', context.params.documentId, original);

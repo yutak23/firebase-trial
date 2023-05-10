0e8d6d5fc9f4aba90802d7dfaeb5bbf25455ff4d
Yuta Katayama <yutapilafu@gmail.com>
2023-05-10 20:28:59 +0900

    chore: 不要なfunctionsを削除

--- firebase-cloud-functions/index.js
+++ firebase-cloud-functions/index.js
@@ -31,30 +31,6 @@ const converter = {
 	}
 };
 
-export const addMessage = functions
-	.region('asia-northeast1')
-	.https.onRequest(async (req, res) => {
-		const original = req.query.text;
-
-		const writeResult = await admin
-			.firestore()
-			.collection('messages')
-			.add({ original });
-
-		res.json({ result: `Message with ID: ${writeResult.id} added.` });
-	});
-
-export const makeUppercase = functions
-	.region('asia-northeast1')
-	.firestore.document('/messages/{documentId}')
-	.onCreate((snap, context) => {
-		const { original } = snap.data();
-		functions.logger.log('Uppercasing', context.params.documentId, original);
-
-		const uppercase = original.toUpperCase();
-		return snap.ref.set({ uppercase }, { merge: true });
-	});
-
 export const createGroupMemberUsers = functions
 	.region('asia-northeast1')
 	.firestore.document('/groups/{documentId}')

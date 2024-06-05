102a1b74b34b3e85aa528f649d50657dcbbb3b5c
yutak23 <yutapilafu@gmail.com>
2024-06-05 16:42:41 +0900

    fix: ESLint error

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,15 +1,11 @@
+/* eslint-disable import/no-unresolved */
 import '@mdi/font/css/materialdesignicons.css';
-// eslint-disable-next-line import/no-unresolved
 import 'vuetify/styles';
 import { createVuetify } from 'vuetify';
-// eslint-disable-next-line import/no-unresolved
 import * as components from 'vuetify/components';
-// eslint-disable-next-line import/no-unresolved
 import * as directives from 'vuetify/directives';
-import colors from 'vuetify/lib/util/colors';
-// eslint-disable-next-line import/no-unresolved
+import colors from 'vuetify/util/colors';
 import * as labsComponents from 'vuetify/labs/components';
-// eslint-disable-next-line import/no-unresolved
 import { createVueI18nAdapter } from 'vuetify/locale/adapters/vue-i18n';
 import { useI18n } from 'vue-i18n';
 import i18n from '@/plugins/i18n';
